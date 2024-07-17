8cb331adae4a2176f264df7b32c0dfcf5bf91868
Frank Hunleth <fhunleth@troodon-software.com>
2024-07-16 22:46:42 -0400

    Update GitHub repository location

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule KioskSystemRpi4.MixProject do
   use Mix.Project
 
-  @github_organization "fhunleth"
+  @github_organization "nerves-web-kiosk"
   @app :kiosk_system_rpi4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
